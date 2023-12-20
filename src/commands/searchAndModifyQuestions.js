a9d490a3335c7c8b278c385549df4edea5c67200
Nicolas <130666872+Nico6003@users.noreply.github.com>
2023-12-20 15:33:50 +0100

    Update searchAndModifyQuestions.js
    Résolution de l'issue de recherche de question.

--- src/commands/searchAndModifyQuestions.js
+++ src/commands/searchAndModifyQuestions.js
@@ -4,14 +4,27 @@ const giftParser = require('../../parsers/giftParser.js');
 const vCardParser = require('../../parsers/vCardParser.js');
 const vg = require('vega');
 const vegalite = require('vega-lite');
+const exam = require('../../services/examService.js');
+const { forEach } = require('vega-lite/build/src/encoding.js');
+
 
 const cli = require("@caporal/core").default;
 
 cli
 	.version('vpf-parser-cli')
 	.version('0.07')
-	
-	
+	.command('search_question', 'Permet de rechercher une question')
+	.argument('<idq>', 'Id de la question à trouver')
+	.action(({ args }) => {
+		const listequestion = exam.allQuestions();
+		listequestion.forEach(question => {
+			if (question.id == idq) {
+				console.log(question);
+			}
+		}
+		);
+	})
 
-	
+
+
 cli.run(process.argv.slice(2));
