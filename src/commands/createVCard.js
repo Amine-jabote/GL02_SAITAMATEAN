dab84d85abfe0d37089c08925a57674766ffea54
Nicolas <130666872+Nico6003@users.noreply.github.com>
2023-12-20 17:08:49 +0100

    Update createVCard.js
    Résolution d'une issue.

--- src/commands/createVCard.js
+++ src/commands/createVCard.js
@@ -9,10 +9,10 @@ const vegalite = require('vega-lite');
 const cli = require("@caporal/core").default;
 
 cli
-    .version('vpf-parser-cli')
-    .version('0.07')
-    
-     
+	.version('vpf-parser-cli')
+	.version('0.07')
+
+
 
 	// create_Vcard
 	.command('create_Vcard', 'Créer le Vcard d\'un professeur')
@@ -25,6 +25,19 @@ cli
 		else console.log('Veuillez d\'abord vous connecter à un compte possédant les droits nécessaires.'.red);
 	})
 
-    
+
 cli.run(process.argv.slice(2));
 
+const prompt = require("prompt-sync")();
+let choice = prompt("Rentrez le nom :");
+
+let choicep = prompt("Rentrez le prénom :");
+
+let choicet = prompt("Rentrez le numéro detéléphone :");
+
+let choicee = prompt("Rentrez l'adresse email :");
+
+let stringEnregistrer = 'BEGIN:VCARD\nVERSION:4.0\nN:${choice}\nFn:${choicep}\nTel;tel:${choicet}\nEMAIL:${choicee}\nEND:VCARD';
+// Enregistrement dans un fichier VCARD 
+fs.writeFileSync('contact.vcard', stringEnregistrer, 'utf-8');
+console.log('Fichier VCard enregistré avec succès.'.green);
