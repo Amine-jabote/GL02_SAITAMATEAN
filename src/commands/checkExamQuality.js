5b399f07a71bc5671370d3edadaf6a51e1aaea8a
Mohamed <122234957+Mohaaz45@users.noreply.github.com>
2024-01-01 21:17:51 +0100

    Modification majeure du module checkExamQuality.js

--- src/commands/checkExamQuality.js
+++ src/commands/checkExamQuality.js
@@ -1,29 +1,70 @@
 
+const readline = require('readline');
 const fs = require('fs');
-const colors = require('colors');
-const giftParser = require('../../parsers/giftParser.js');
-const vCardParser = require('../../parsers/vCardParser.js');
-const vg = require('vega');
-const vegalite = require('vega-lite');
+const { Console } = require('console');
+const { rawListeners } = require('process');
+var result=[];
 
-const cli = require("@caporal/core").default;
+const r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-cli
-    .version('vpf-parser-cli')
-    .version('0.07')
-    
-     
-    	//submit
-	.command('submit', 'Contrôle la qualité d\'un examen')
-	.argument('<nomExam>', 'le nom de l\'examen')
-	.action(({ args }) => {
-		var utilisateur = new Utilisateur();
-		utilisateur = utilisateur.findConnected();
-		if (utilisateur.userName !== undefined && utilisateur.type === 'P') {
-			var examen = new Examen();
-			quality = examen.submit(args.nomExam)
-		}
-		else console.log('Veuillez d\'abord vous connecter à un compte possédant les droits nécessaires.'.red)
-	})
 
-cli.run(process.argv.slice(2));
+const dir = './Forms'
+let path = 'Forms/EM-U5-p34-Voc.gift'
+
+function verifyTest(questions) {
+    let correct = true;
+    const titleRegex = /^::.+::/g;
+    console.log(questions);
+    for (let i = 0; i < questions.length; i++) {
+        if (questions[i] != undefined && questions[i] != '') {
+            let array = questions[i].trim()
+            result.push(array)
+            if (questions[i].match(titleRegex) != null)
+                continue;
+            else
+                correct = false;
+        }
+    }
+    // questions.forEach(question =>{
+    //     if(question.match(/{[^}]*}/g)) correct = false;
+    // })
+
+    // questions.forEach(question => {
+    // if (questions.filter(q => q.title == question.split(titleRegex).length > 1)) {
+    //     correct = false;
+    //     }
+    // });
+    if (result.length < 15 || result.length > 20) {
+
+        correct = false;
+    }
+    console.log(result.length)
+    return correct;
+
+}
+
+
+fs.readdir('./Forms/', function (err, readDir) {
+    if (err) {
+        throw err;
+    }
+    for (let i = 0; i < readDir.length; i++) {
+        console.log(readDir[i]);
+    }
+    r.question('Enter the name of the form ', function (name) {
+        fs.readFile('./Forms/' + name, function read(err, data) {
+            if (err) {
+                throw err;
+            }
+            const document = data.toString();
+            let questions = (document.split('\n\n'));
+            questions.shift();
+
+            console.log(verifyTest(questions));
+            r.close();
+        });
+    })
+})
